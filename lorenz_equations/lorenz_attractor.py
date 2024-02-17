03cf8ac6e80a6627785f967ad5d1a28bfcf0f457
garishka <gergana.daskalova01@gmail.com>
2024-02-17 02:38:23 +0200

    cute графики

---
+++ lorenz_equations/lorenz_attractor.py
@@ -0,0 +1,42 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from typing import Union
+from scipy.integrate import solve_ivp
+
+
+def lorenz_equations(t, xyz: Union[list, np.ndarray], sigma: float, r: float, b: float):
+    if isinstance(xyz, list):
+        x, y, z = xyz
+    elif isinstance(xyz, np.ndarray):
+        x = xyz[0]
+        y = xyz[1]
+        z = xyz[2]
+    else:
+        raise TypeError("Check the input type of xyz, smh...")
+
+    if len(xyz) != 3:
+        raise ValueError("Why tf is xyz so long?")
+
+    dxdt = sigma * (y - x)
+    dydt = r * x - y - x * z
+    dzdt = x * y - b * z
+    return np.array([dxdt, dydt, dzdt])
+
+
+solution = solve_ivp(fun=lorenz_equations,
+                     t_span=(0, 100),
+                     t_eval=np.linspace(0, 100, 50_000),
+                     y0=[0., 1., 1.05],
+                     method="DOP853",
+                     dense_output=True,
+                     args=(10, 28, 2.667))
+
+ax = plt.figure().add_subplot(projection='3d')
+
+ax.plot(*solution.y, lw=0.5)
+ax.set_xlabel("X Axis")
+ax.set_ylabel("Y Axis")
+ax.set_zlabel("Z Axis")
+ax.set_title("Lorenz Attractor")
+
+plt.show()
